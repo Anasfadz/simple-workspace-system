2751af8eaf35ba2f21dc8af037abd1faeb5c4446
Anasfadz <anasfadzrin@gmail.com>
2025-12-29 11:28:58 +0800

    add portfolio

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WorkspaceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => redirect()->route('workspaces.index'));
+Route::get('/', fn() => view('portfolio'))->name('home');
 
 Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', fn() => redirect()->route('workspaces.index'))->name('dashboard');
     Route::resource('workspaces', WorkspaceController::class);
 
     Route::resource('workspaces.tasks', TaskController::class)
